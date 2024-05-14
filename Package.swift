6bd63c4a50121ce2e7c7adc3b8afe510a47a77c1
intaek.cho <intaek.cho@navercorp.com>
2024-05-14 14:01:06 +0900

    arm64 지원 - SPM 배포 가능하도록 프로젝트 수정
    - 헤더 파일들 include 디렉토리로 이동
    - 불필요한 swift 파일 제거(동일 이름의 objc 파일이 있음)

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.7.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "XbICalendar",
+    defaultLocalization: "en",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "XbICalendar",
+            targets: ["XbICalendar"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "XbICalendar",
+            path: "XbICalendar/XBICalendar", 
+            publicHeadersPath: "include"),
+        .testTarget(
+            name: "XbICalendarTests",
+            dependencies: ["XbICalendar"],
+            path: "XbICalendar/XbICalendarTests")
+    ]
+)
